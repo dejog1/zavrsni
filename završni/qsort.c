a608da97ef06a3d29dd2a8982f6ebd87a224bfa6
dejog1 <dejangrabovac1@gmail.com>
2024-06-18 18:03:59 +0200

    Update qsort.c

--- završni/qsort.c
+++ završni/qsort.c
@@ -9,18 +9,18 @@
 
 
 int comparePlayers(const void* a, const void* b) {
-  
     const Player* playerA = (const Player*)a;
     const Player* playerB = (const Player*)b;
 
-   
-    return strcmp(playerA->name, playerB->name);
+    if (playerA->wins > playerB->wins) return -1;
+    if (playerA->wins < playerB->wins) return 1;
+    return 0;
 }
 
 void sortPlayersByWins(Game* game) {
     if (game == NULL) return;
     Player players[MAX_PLAYERS] = { game->player1, game->player2 };
-    qsort(players, MAX_PLAYERS, sizeof(Player), comparePlayers); //21., 23., 24.
+    qsort(players, MAX_PLAYERS, sizeof(Player), comparePlayers);
 
     printf("Igraci sortirani po broju pobjeda:\n");
     for (int i = 0; i < MAX_PLAYERS; i++) {
